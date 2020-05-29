972ac1c6823bd63b9028752439ea7140bb5aba24
孙凯 <sunkai@qq,com>
2020-05-29 11:09:31 +0800

    修改忽略文件配置

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    'dbConfig': {
+        host: '127.0.0.1',
+        user: 'root', // mysql的账号
+        password: '123456', // mysql 的密码
+        database: 'mybookmarks',
+        multipleStatements: true,
+        useConnectionPooling: true,
+        port: 3306
+    }
+}
