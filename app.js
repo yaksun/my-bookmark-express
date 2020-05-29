505b0557339695f82d2f1165570c38f20e1c18d2
孙凯 <sunkai@qq,com>
2020-05-29 10:17:27 +0800

    注销redis密码

--- app.js
+++ app.js
@@ -31,12 +31,12 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(cookieParser('secret'));
-
+// 线上不需要redis密码auth_pass:'123456'
 app.use(session({
   store: new RedisStore({
     host: "127.0.0.1",
-    port: 6379,
-    auth_pass:'123456'
+    port: 6379
+  
   }),
   cookie: {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 一周
