187d366032bcf8f353daf01b7af08975390a8b0c
yaksun <sunk@gzjp.cn>
2023-02-23 11:13:13 +0800

    分支最新版

--- app.js
+++ app.js
@@ -39,7 +39,8 @@ app.use(cookieParser('secret'));
 app.use(session({
   store: new RedisStore({
     host: "127.0.0.1",
-    port: 6379
+    port: 6379,
+    // auth_pass:'123456'
   
   }),
   cookie: {
--- config.default.js
+++ config.default.js
@@ -1,11 +1,13 @@
 module.exports = {
     'dbConfig': {
+        // host: '139.224.195.82',
         host: '127.0.0.1',
         user: 'root', // mysql的账号
-        password: '123456', // mysql 的密码
+        password: '92mb30', // mysql 的密码
         database: 'mybookmarks',
         multipleStatements: true,
         useConnectionPooling: true,
-        port: 3306
+        // port: 8081,
+        port:3306
     }
 }
